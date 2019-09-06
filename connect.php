81a819e05e4b086eb506935eb5f7e6a3b38df48c
zgoodman1 <zgoodman@live.unc.edu>
2019-09-06 15:01:45 -0400

    make connection php file and bsaic index file for registration page

---
+++ connect.php
@@ -0,0 +1,63 @@
+<?php
+$username = filter_input(INPUT_POST, 'username');
+$email = filter_input(INPUT_POST, 'email');
+$password = filter_input(INPUT_POST, 'password'); 
+$cc_num = filter_input(INPUT_POST, 'cc_num');
+
+if(!empty($username)){
+    if(!empty($email)){
+        if(!empty($password)){
+            if(!empty($cc_num)){
+                $host = "localhost";
+                $dbusername = "root";
+                $dpasssword = "";
+                $dbname = "test";
+
+                $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+                if(mysqli_connect_error()){
+                    die('Connect Error ('. mysqli_connect_errno() .')'. mysqli_connect_error();
+                }
+                else{
+                    $sql = "INSERT INTO bank_app_info (user_id, user_email, user_pw, user_cc_num)
+                    values = ('$username', '$email', '$password", '$cc_num')";
+                    if($conn->query($sql)){
+                        echo "New user registered successfully";
+                    }
+                    else{
+                        echo "Error: ". $sql ."<br>". $conn->error;
+                    }
+                    $conn->close();
+                }
+            }
+            else {
+                echo "Please enter a 16 digit card number (a fake one, please don't enter your own)";
+                die();
+            }
+        }
+        else {
+            echo "Password should not be empty";
+            die();
+        }
+    }
+    else {
+        echo "Email should not be empty";
+        die();
+    }
+}
+else {
+    echo "Username should not be empty";
+    die();
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
