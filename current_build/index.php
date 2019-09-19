7782e366afeedd36fb597dc14ba1c746421ca86e
johnfeshuk <johnfesh717@gmail.com>
2019-09-19 16:26:56 -0400

    Redesigning webpage flow from homepage
    changes to index.php
    changes to css styles used
    added header

--- current_build/index.php
+++ current_build/index.php
@@ -1,46 +1,37 @@
 <html>
-    <head>
-        <meta charset="ut-8">
-        <title>Home</title>
-        <link rel='stylesheet' href='css/style.css'/>
-        <link rel="stylesheet" type="text/css"
-        href="styles.css">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel = "stylesheet" type = "text/css" href = "teststyle.css" />
 </head>
-<body>
-    <h1>
-        <?php
-         require('config.php');
-         session_start();
-     
-         $currUsername = $_SESSION['username'];
-     
-         echo "Welcome, " . $currUsername . "</br>";
-        ?>
-    </h1>
-    <h2>
-        <?php 
-        $currUsername = $_SESSION['username'];
-        $sql = "SELECT user_balance FROM bank_app_info_2 WHERE user_id = '$currUsername'";
-        $balance = $link->query($sql);
-        if($row = $balance->fetch_assoc()){
-            echo "</br>Balance: " . $row['user_balance'] . "</br>";
-        }
-        ?>
-    </h2>
-    
-    <h3>
-        <?php
-        echo "<div class='form'>
-        <br/>Click here to <a href='deposit.php'>Make a Deposit</a></div>";
-        ?>
-    </h3>
-    
-    <h4>
-        <?php
-        echo "<div class='form'>
-        <br/>Click here to <a href='logout.php'>Log Out</a></div>";
-        ?>
-</body>
-</html>
-    
 
+<body>
+    <div class="header">
+        <a href="index.php" class="logo">Bank of InfoSec</a>
+        <div class="header-right">
+            <a class="active" href="#home">Home</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+    </div>
+
+    <div style="padding-left:20px">
+
+        <h2>
+            <?php
+            echo "<div class='form'>
+            <br/>Click here to <a href='login.php'>Login</a></div>";
+            ?>
+        </h3>
+
+        <h2>
+            <?php
+            echo "<div class='form'>
+            <br/>Click here to <a href='register.php'>Register</a></div>";
+            ?>
+        </h3>
+
+    </div>
+
+</body>
+
+</html>
