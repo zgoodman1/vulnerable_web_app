c196a59b0df452c7c49f8287b63931f44549efde
zgoodman1 <zgoodman@live.unc.edu>
2019-12-02 13:18:09 -0500

    fixed user registration to insert new users to db

--- current_build/logout.php
+++ current_build/logout.php
@@ -1,15 +1,10 @@
 <?php
-<<<<<<< HEAD
-// start and then destory session
-session_start();
-=======
 //initialize session
 require('config.php');
 session_start();
 //unset all session variables
 $_SESSION = array();
 //destroy the session
->>>>>>> jwfeshuk-master
 session_destroy();
 //redirect to index page
 header("location: index.php");
--- current_build/register.php
+++ current_build/register.php
@@ -17,33 +17,47 @@
     <div class="header">
         Register
     </div>
-
-    <form method="post" action="register.php">
-        <?php include('errors.php'); ?>
-        <div class="input-group">
-            <label>Username</label>
-            <input type="text" name="username" value="<?php echo $username; ?>">
-        </div>
-        <div class="input-group">
-            <label>Email</label>
-            <input type="email" name="email" value="<?php echo $email; ?>">
-        </div>
-        <div class="input-group">
-            <label>Password</label>
-            <input type="password" name="password_1">
-        </div>
-        <div class="input-group">
-            <label>Confirm password</label>
-            <input type="password" name="password_2">
-        </div>
-        <div class="input-group">
-            <button type="submit" class="btn" name="reg_user">Register</button>
-        </div>
-        <p>
-            Already a member? <a href="login.php">Sign in</a>
-        </p>
-    </form>
-
+    <div class="content">
+        <?php
+        require('config.php');
+        // if form is submitted, insert values to the database
+        if (isset($_REQUEST['username'])) {
+            // removes backslashes
+            $username = stripslashes($_REQUEST['username']);
+            // escapes special chars in a str
+            $username = mysqli_real_escape_string($link, $username);
+            $email = stripslashes($_REQUEST['email']);
+            $email = mysqli_real_escape_string($link, $email);
+            $password = stripslashes($_REQUEST['password']);
+            $passwrod = mysqli_real_escape_string($link, $password);
+            $bal = 0;
+            //$cc_num =  stripslashes($_REQUEST['cc_num']);
+            //$cc_num = mysqli_real_escape_string($link, $cc_num);
+            $query = "INSERT into `users` (username, email, password, 
+            balance) VALUES ('$username', '$email', '$password', '$bal')";
+            $result = mysqli_query($link, $query);
+            if ($result) {
+                echo "<div class='form'>
+                <h3>You have been registered successfully.</h3>
+                <br/>Click here to <a href='login.php'>Login</a></div>";
+            }
+        } else {
+            ?>
+            <div class="form-container">
+                <div class="user-img"></div>
+                <h3>Registration</h3>
+                <form method="post">
+                        <div class='input-group'><input type="text" name="username" placeholder="Username"></div>
+                        <div class='input-group'><input type="email" name="email" placeholder="Email"></div>
+                        <div class='input-group'><input type="password" name="password" placeholder="Password" class="form-control"></div>
+                        <div class='input-group'><input type="password" name="password_confirm" class="form-control" placeholder="Confirm Password"></div>
+                        <div class='input-group'><input type="submit" class="btn btn-primary" value="Submit"></div>
+                </form>
+                <div class='form'>
+                    Already have an account? <a href='login.php'> Login here</a>
+                </div>
+            </div>
+        <?php } ?>
 </body>
 
 </html>
