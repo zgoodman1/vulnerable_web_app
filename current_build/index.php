ac95709f7b485437565e3631f98161c32a217260
zgoodman1 <zgoodman@live.unc.edu>
2019-12-02 11:26:15 -0500

    tweaks

--- current_build/index.php
+++ current_build/index.php
@@ -9,13 +9,8 @@
 <body>
 
     <header>
-<<<<<<< HEAD
-        <a href='index.php'><button type='button'>Main</button></a></li>    
-        <a href='home.php'><button type='button'>Home</button></a></li>   
-=======
         <a href='index.php'><button type='button'>Main</button></a></li>
         <a href='home.php'><button type='button'>Home</button></a></li>
->>>>>>> jwfeshuk-master
     </header>
 
     <div class="header">
@@ -28,23 +23,6 @@
         <div class="link"><a href='register.php'>Register Here</a></div>
     </div>
 
-<<<<<<< HEAD
-    <!--
-    <div class="form"></div>
-    <h1>Welcome to InfoSec Bank</h1>
-    <h2><i>"Where your security is our priority!"</i></h2>
-    <h3>
-        <?php
-        /*
-        echo "<p><br/><a href='login.php'>Log In Here</a></p><br/>";
-        echo "<p><a href='register.php'>Register Here</a></p>";
-        */
-        ?>
-    </h3>
--->
-
-=======
->>>>>>> jwfeshuk-master
 </body>
 
 </html>
